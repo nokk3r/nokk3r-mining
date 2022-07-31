8ec11a54378476a276121f403d822f44321784f8
nokk3r <nokk3r@mail.ru>
2022-07-31 17:41:46 +1100

    first init
    Initialization react app with:
    - typescript
    - prettier
    - eslint

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.scss"
+
+function App() {
+    return (
+        <div className="App">
+            <header className="app_header">
+                <h2>Application works!</h2>
+            </header>
+        </div>
+    )
+}
+
+export default App
